605be2283b0c7d140d84992a124744540f789cc5
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2018-06-20 09:01:26 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#[macro_use] extern crate honggfuzz;
+extern crate lewton;
+
+use std::io::Cursor;
+use std::result::Result;
+
+fn main() {
+    // Here you can parse `std::env::args and 
+    // setup / initialize your project
+
+    // You have full control over the loop but
+    // you're supposed to call `fuzz` ad vitam aeternam
+    loop {
+        // The fuzz macro gives an arbitrary object (see `arbitrary crate`)
+        // to a closure-like block of code.
+        // For performance reasons, it is recommended that you use the native type
+        // `&[u8]` when possible.
+        // Here, this slice will contain a "random" quantity of "random" data.
+        fuzz!(|data: &[u8]| {
+            let cursor = Cursor::new(data);
+
+            if let Ok(mut reader) = lewton::inside_ogg::OggStreamReader::new(cursor) {
+                while let Ok(Some(_)) = reader.read_dec_packet() {}
+            }
+        });
+    }
+}
